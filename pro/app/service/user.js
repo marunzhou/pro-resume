92cbbbf92f0f5ea6e807e5c3982d3376da300d1e
marunzhou <522950012@qq.com>
2018-12-07 13:19:47 +0800

    转化为utf8

--- pro/app/service/user.js
+++ pro/app/service/user.js
@@ -2,13 +2,13 @@ const moment = require('moment');
 const Service = require('egg').Service;
 
 class UserService extends Service {
-    // 查询帐户信息
+    // 鏌ヨ甯愭埛淇℃伅
     async find(id) {
         const user = await this.app.mysql.get('user', { id });
         return { user };
     }
 
-    // 添加帐户
+    // 娣诲姞甯愭埛
     async insert(obj) {
         const values = {
             username: '',
@@ -21,7 +21,7 @@ class UserService extends Service {
         return result.affectedRows === 1;
     }
 
-    // 更新用户信息
+    // 鏇存柊鐢ㄦ埛淇℃伅
 
 }
 
